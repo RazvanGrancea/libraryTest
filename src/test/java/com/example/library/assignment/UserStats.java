29c179d0548e4a314cd3a136e6dc4d8412d437de
sorinvisan89 <sorin_andrei_visan@yahoo.com>
2024-08-22 19:49:40 +0300

    Testing

--- src/test/java/com/example/library/assignment/UserStats.java
+++ src/test/java/com/example/library/assignment/UserStats.java
@@ -4,6 +4,9 @@ import java.util.*;
 import lombok.*;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserStats {
 
   private Optional<Long> visitCount;
