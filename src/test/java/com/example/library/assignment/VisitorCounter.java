214d28f1dbcf315657a91d92f150df997a66d28c
sorinvisan89 <sorin_andrei_visan@yahoo.com>
2024-08-22 19:39:34 +0300

    Testing

--- src/test/java/com/example/library/assignment/VisitorCounter.java
+++ src/test/java/com/example/library/assignment/VisitorCounter.java
@@ -12,14 +12,15 @@ public class VisitorCounter {
 
     return Arrays.stream(stats)
         .filter(Objects::nonNull) // filter out null maps (null elements from the array)
+        .filter(map -> !map.isEmpty()) // filter out maps that are empty
         .flatMap(map -> map.entrySet().stream())  // flatten the list of maps into a stream of entries
-        .filter(entry -> isParsableToLong(entry.getKey())) // filter String keys that are not parseable to Longs
-        .filter(entry -> entry.getValue() != null) // filter UserStats values that are null
-        .filter(entry -> entry.getValue().getVisitCount().isPresent()) // filter UserStats values that are absent
+        .filter(entry -> isParsableToLong(entry.getKey())) // filter out String keys that are not parseable to Longs
+        .filter(entry -> entry.getValue() != null) // filter out UserStats values that are null
+        .filter(entry -> entry.getValue().getVisitCount().isPresent()) // filter out UserStats values that are absent
         .collect(Collectors.toMap(
             entry -> Long.parseLong(entry.getKey()), // map String keys to Long, these are the final user ids longs
             entry -> entry.getValue().getVisitCount().get(), // map UserStats to visitCount Long value
-            Long::sum // Sum the counts for duplicate keys (for the same userId)
+            Long::sum // Sum the counts for duplicate keys (for the same userId). This is the duplicate key remapping function.
         ));
   }
 
