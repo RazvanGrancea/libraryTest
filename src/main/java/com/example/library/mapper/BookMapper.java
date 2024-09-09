2e18355abd430a9ae3c6727b6e84d55350216db5
sorinvisan89 <sorin_andrei_visan@yahoo.com>
2024-09-09 21:14:25 +0300

    Fixes

--- src/main/java/com/example/library/mapper/BookMapper.java
+++ src/main/java/com/example/library/mapper/BookMapper.java
@@ -3,18 +3,15 @@ package com.example.library.mapper;
 import com.example.library.dto.BookRequestDTO;
 import com.example.library.dto.BookResponseDTO;
 import com.example.library.model.Book;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface BookMapper {
 
     @Mapping(source = "author.id", target = "authorId")
-    @Mapping(source = "description", target = "description")
     BookResponseDTO toDTO(Book book);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(source = "authorId", target = "author.id")
-    @Mapping(source = "description", target = "description")
     Book toEntity(BookRequestDTO dto);
 }
